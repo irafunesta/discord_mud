e11e16dd950cd65f19a428bf9016f9aeb03c4cc8
Guidi Simone <gsimone21892@gmail.com>
2017-11-20 21:11:27 +0100

    Added commands: login getUser help

--- index.js
+++ index.js
@@ -11,18 +11,20 @@ const client = new Discord.Client();
 const token = 'MzgxNzk0NDM4NDE2MDM5OTM3.DPMZuA.m6brNk90VQMDHk_gMTLedJNS77k';
 
 //Lokydb
-var db = new loki('./data/loki.json', {
+var db = new loki('./data/game.json', {
     autoload: true,
     autoloadCallback : databaseInitialize,
     autosave: true,
     autosaveInterval: 4000
 });
 
-function databaseInitialize() {
-	var accounts = db.getCollection("accounts");
+var characters = null;
 
-	if (accounts === null) {
-		accounts = db.addCollection("accounts");
+function databaseInitialize() {
+	characters = db.getCollection("characters");
+
+	if (characters === null) {
+		characters = db.addCollection("characters");
 	}
 
 	// var userDoc = {
@@ -78,6 +80,44 @@ function Run() {
 					var res = db.getCollection("accounts").find({});
 					console.log(res);
 					message.channel.send('res:' + JSON.stringify(res));
+				break;
+				case 'getChar':
+					//!g:createAccount:name:pswd:emal
+					var userid = message.author.id;
+					var char = characters.findOne({user_id:userid});
+					console.log(JSON.stringify(char));
+
+					message.channel.send(JSON.stringify(char));
+				break;
+				case 'login':
+					//Check if the user has a char or make a new one
+					//need to pass the name of the char
+					var userid = message.author.id;
+					var char = characters.findOne({user_id:userid});
+					if(char) {
+						//Greete the create character
+						message.channel.send("Welcome back to the world " + char.name);
+					}
+					else if(arr[2]) {
+						//Make new char
+						var charName = arr[2];
+						characters.insert({
+							user_id: userid,
+							name:charName //TODO add other things
+						});
+						message.channel.send("Welcome to the world " + charName +
+							' !. \n Type !g:help for a list of command. Have fun');
+					}
+					else {
+						//Error need a name of the character
+						message.channel.send("The login command need a name as a parameter.");
+					}
+				break;
+				case 'help':
+					message.channel.send("Work in progress");
+				break;
+				default:
+					message.channel.send("Wrong command type !g:help for a list of command.");
 				break;
 			}
 		}
